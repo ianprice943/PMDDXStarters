b1d6c757d266d8af62b79d45cda64d3a01867f73
= <ianprice943@yahoo.com>
2020-04-27 23:46:25 -0400

    Update routing

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App.vue'
 import routes from './routes'
 import store from './store'
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
-const router = new VueRouter({routes});
+const router = new VueRouter({routes, mode: 'history'});
 
 new Vue({
   router,
