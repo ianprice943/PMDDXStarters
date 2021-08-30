1383d3101b08a90ab0c167900dc2d21fc4d9507e
= <ianprice943@yahoo.com>
2021-08-30 15:29:24 -0400

    added link to React.js version of this site

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
     <span>Written in VueJS by Ian Price. </span>
     <span v-if="showEmail">Please email me your feedback and suggestions at captainprice943@gmail.com. </span>
     <span>This website is in no way involved with The Pokemon Company, Gamefreak, or Nintendo. </span>
+    <a href="https://pmdrtdx-react.herokuapp.com/">React.js Version</a>
   </div>
 </template>
 
@@ -23,5 +24,12 @@
     font-size: .75em;
     padding-top: 1em;
     color: #EEEEEE;
+  }
+  #footer-content > a {
+    text-decoration: underline;
+  }
+  #footer-content > a:visited {
+    color: #EEE;
+    text-decoration: underline;
   }
 </style>
